04bd8c227a515e9e0259e4d99aeb0340341b7ed7
Matt Woerman <mwoerman@umass.edu>
2020-09-24 14:12:16 -0400

    week 6

--- readme.md
+++ readme.md
@@ -28,6 +28,9 @@ Week 5: **Logit Model**
 * *No new materials*
 
 Week 6: **Maximum Likelihood Estimation**
+* [Notes](https://raw.githack.com/woerman/ResEcon703/master/notes/mle/mle.pdf) | [Slides](https://raw.githack.com/woerman/ResEcon703/master/slides/week_06/week_06.pdf) | [R code](https://raw.githack.com/woerman/ResEcon703/master/slides/week_06/week_06.R) | [Data](https://raw.githack.com/woerman/ResEcon703/master/slides/week_06/ac_renters.csv)
+
+* Videos: [1](https://www.youtube.com/watch?v=12M8wJT3GR8&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [2](https://www.youtube.com/watch?v=H0EPloe8qhs&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [3](https://www.youtube.com/watch?v=fF9yhX2Q8CQ&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [4](https://www.youtube.com/watch?v=kKYY_OXGHxY&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [5](https://www.youtube.com/watch?v=esY21bNZEEg&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [6](https://www.youtube.com/watch?v=E0B-vSIT0Nc&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [7](https://www.youtube.com/watch?v=Y9WgOTwQYXw&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal)
 
 Week 7: **Logit Estimation**
 
---
+++ slides/week_06/week_06.R
@@ -0,0 +1,96 @@
+########################################################
+##### ResEcon 703: Topics in Advanced Econometrics #####
+#####    Week 6: Maximum Likelihood Estimation     #####
+#####         Matt Woerman, UMass Amherst          #####
+########################################################
+
+### MLE Regression Example -----------------------------------------------------
+
+### Examine the mtcars dataset
+## Load tidyverse
+library(tidyverse)
+## Look at the mtcars data
+tibble(mtcars)
+## Summarize the mtcars dataset
+mtcars %>% 
+  select(mpg, disp, hp, wt, qsec) %>% 
+  summary()
+## Plot the mtcars dataset
+ggplot(data = mtcars, mapping = aes(x = hp, y = mpg)) +
+  geom_point()
+
+### Optimization in R
+## Help file for the optimization function, optim
+?optim
+## Arguments for optim function
+optim(par, fn, gr, ..., method, lower, upper, control, hessian)
+
+### Estimate regression by MLE using numerical optimization
+## Create function to calculate OLS log-likelihood
+ll_ols <- function(params, data, y_var, x_vars) {
+  ## Add column of ones for the constant term
+  reg_data <- data %>% 
+    mutate(constant = 1)
+  ## Select data for X and convert to a matrix
+  X <- reg_data %>% 
+    select(all_of(c('constant', x_vars))) %>% 
+    as.matrix()
+  ## Select data for y and convert to a matrix
+  y <- reg_data %>% 
+    select(all_of(y_var)) %>% 
+    as.matrix()
+  ## Select coefficient parameters
+  beta_hat <- params[-length(params)]
+  ## Select error variance parameters
+  sigma2_hat <- params[length(params)]
+  ## Calculate fitted y values
+  y_hat <- X %*% beta_hat
+  ## Calculate the pdf values of each outcome
+  y_pdf <- dnorm(y, mean = y_hat, sd = sqrt(sigma2_hat))
+  ## Calculate the log-likelihood
+  ll <- sum(log(y_pdf))
+  ## Return the negative of log-likelihood for minimization
+  return(-ll)
+}
+## Maximize the OLS log-likelihood function
+mle_ols_1 <- optim(par = c(0, 0, 1), fn = ll_ols, 
+                   data = mtcars, y_var = 'mpg', x_vars = 'hp',
+                   method = 'BFGS', hessian = TRUE)
+## Show optimization results
+mle_ols_1
+## Show parameter estimates
+mle_ols_1$par
+## Calculate MLE standard errors
+mle_ols_1$hessian %>% 
+  solve() %>% 
+  diag() %>% 
+  sqrt()
+
+### Estimate regression of mpg on disp, wt, and qsec using MLE
+## Maximize the OLS log-=likelihood function
+mle_ols_2 <- optim(par = c(rep(0, 5), 1), fn = ll_ols, 
+                   data = mtcars, y_var = 'mpg', 
+                   x_vars = c('hp', 'disp', 'wt', 'qsec'),
+                   method = 'BFGS', hessian = TRUE)
+## Show parameter estimates
+mle_ols_2$par
+## Calculate MLE standard errors
+mle_ols_2$hessian %>% 
+  solve() %>% 
+  diag() %>% 
+  sqrt()
+
+### Estimate regression of Petal.Length on other iris variables using MLE
+## Maximize the OLS log-=likelihood function
+mle_ols_3 <- optim(par = c(rep(0, 4), 1), fn = ll_ols, 
+                   data = iris, y_var = 'Petal.Length', 
+                   x_vars = c('Petal.Width', 'Sepal.Length',
+                              'Sepal.Width'),
+                   method = 'BFGS', hessian = TRUE)
+## Show parameter estimates
+mle_ols_3$par
+## Calculate MLE standard errors
+mle_ols_3$hessian %>% 
+  solve() %>% 
+  diag() %>% 
+  sqrt()
