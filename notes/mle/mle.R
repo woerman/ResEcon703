6dd06dce96be277df2d634b2f3103f9d600f94e7
Matt Woerman <mwoerman@umass.edu>
2021-10-07 16:52:31 -0400

    week 6

---
+++ notes/mle/mle.R
@@ -0,0 +1,120 @@
+########################################################
+##### ResEcon 703: Topics in Advanced Econometrics #####
+#####      Create examples for MLE supplement      #####
+#####         Matt Woerman, UMass Amherst          #####
+########################################################
+
+### Load packages --------------------------------------------------------------
+
+library(tidyverse)
+
+
+### Maximum Likelihood Intuition -----------------------------------------------
+
+### Create example for maximum likelihood intuition
+## Set seed for replication
+set.seed(703)
+## Draw five random variables from a normal (50, 5)
+y <- rnorm(5, 50, 1)
+## Round for easier presentation
+y <- round(y, 1)
+y
+## Calculate likelihood of drawing these variables from 0 mean
+prod(dnorm(y, 0, 1))
+## Calculate likelihood of drawing these variables from 0 mean
+prod(dnorm(y, 50, 1))
+
+
+### Probability Density Function -----------------------------------------------
+
+### Plot a probability density function of a standard normal distribution
+## Plot density of standard normal
+norm_pdf <- tibble(x = seq(-5, 5, .01), y = dnorm(x)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_line() +
+  xlab(NULL) +
+  ylab(NULL)
+## Output density plot
+ggsave('norm_pdf.pdf', norm_pdf, width = 5, height = 3)
+
+
+### Maximum Likelihood Examples ------------------------------------------------
+
+### Construct MLE example for Poisson
+## Plot densities of Poisson distributions
+pois_1_pmf <- tibble(x = 0:12, y = dpois(x, 1)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_point() +
+  scale_x_continuous(name = NULL, limits = c(0, 12), breaks = seq(0, 12, 2)) +
+  scale_y_continuous(name = NULL, limits = c(0, .37), breaks = seq(0, .3, .1))
+pois_3_pmf <- tibble(x = 0:12, y = dpois(x, 3)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_point() +
+  scale_x_continuous(name = NULL, limits = c(0, 12), breaks = seq(0, 12, 2)) +
+  scale_y_continuous(name = NULL, limits = c(0, .37), breaks = seq(0, .3, .1))
+pois_5_pmf <- tibble(x = 0:12, y = dpois(x, 5)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_point() +
+  scale_x_continuous(name = NULL, limits = c(0, 12), breaks = seq(0, 12, 2)) +
+  scale_y_continuous(name = NULL, limits = c(0, .37), breaks = seq(0, .3, .1))
+## Output density plot
+ggsave('pois_1_pmf.pdf', pois_1_pmf, width = 3, height = 3)
+ggsave('pois_3_pmf.pdf', pois_3_pmf, width = 3, height = 3)
+ggsave('pois_5_pmf.pdf', pois_5_pmf, width = 3, height = 3)
+## Draw from a Poisson distribution with lambda = 2
+y_pois <- rpois(10, 2)
+y_pois
+## Estimate lambda as the mean
+mean(y_pois)
+## Calculate components of likelihood and log-likelihood functions
+sum(y_pois)
+prod(factorial(y_pois))
+sum(log(factorial(y_pois)))
+## Function to calculate Poisson likelihood for a parameter
+pois_like <- function(data, parameter){
+  output <- dpois(data, parameter) %>% 
+    prod()
+  return(output)
+}
+## Function to calculate Poisson log-likelihood for a parameter
+pois_ll <- function(data, parameter){
+  output <- dpois(data, parameter) %>% 
+    log() %>% 
+    sum()
+  return(output)
+}
+## Calculate likelihood for a range of lambda parameters
+poisson_fit <- tibble(parameter = seq(0, 3, 0.01)) %>% 
+  mutate(like = map_dbl(.x = .$parameter,
+                        .f = pois_like,
+                        data = y_pois),
+         ll = map_dbl(.x = .$parameter,
+                      .f = pois_ll,
+                      data = y_pois))
+## Plot likelihood
+like_plot <- poisson_fit %>% 
+  ggplot(aes(x = parameter, y = like)) + 
+  geom_line() +
+  xlab('Parameter') +
+  ylab('Likelihood') +
+  geom_vline(xintercept = 1.3, linetype = 'dashed')
+## Save likelihood plot
+ggsave('likelihood.pdf', like_plot, width = 3, height = 3)
+## Plot log-likelihood
+ll_plot <- poisson_fit %>% 
+  ggplot(aes(x = parameter, y = ll)) + 
+  geom_line() +
+  xlab('Parameter') +
+  ylab('Log-likelihood') +
+  geom_vline(xintercept = 1.3, linetype = 'dashed')
+## Save likelihood plot
+ggsave('log_likelihood.pdf', ll_plot, width = 3, height = 3)
+
+### Construct MLE example for normal
+## Draw from a normal distribution with (5, 2)
+y_norm <- rnorm(5, 5, 2)
+round(y_norm, 2)
+## Estimate mu as the mean
+mean(y_norm)
+## Estimate sigma^2 as the sample variance
+mean((y_norm - mean(y_norm))^2)
--- readme.md
+++ readme.md
@@ -31,8 +31,8 @@ Week 5: **Logit Model**
 * *No new materials*
 
 Week 6: **Maximum Likelihood Estimation**
-<!-- * [Notes](https://raw.githack.com/woerman/ResEcon703/master/notes/mle/mle.pdf) | [Slides](https://raw.githack.com/woerman/ResEcon703/master/slides/week_06/week_06.pdf) | [R code](https://raw.githack.com/woerman/ResEcon703/master/slides/week_06/week_06.R)
-* Videos: [1](https://www.youtube.com/watch?v=12M8wJT3GR8&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [2](https://www.youtube.com/watch?v=H0EPloe8qhs&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [3](https://www.youtube.com/watch?v=fF9yhX2Q8CQ&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [4](https://www.youtube.com/watch?v=kKYY_OXGHxY&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [5](https://www.youtube.com/watch?v=esY21bNZEEg&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [6](https://www.youtube.com/watch?v=E0B-vSIT0Nc&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [7](https://www.youtube.com/watch?v=Y9WgOTwQYXw&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) -->
+[Notes](https://raw.githack.com/woerman/ResEcon703/master/notes/mle/mle.pdf) | [Slides](https://raw.githack.com/woerman/ResEcon703/master/slides/week_06/week_06.pdf) | [R code](https://raw.githack.com/woerman/ResEcon703/master/slides/week_06/week_06.R)
+* Videos: [1](https://www.youtube.com/watch?v=12M8wJT3GR8&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [2](https://www.youtube.com/watch?v=H0EPloe8qhs&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [3](https://www.youtube.com/watch?v=fF9yhX2Q8CQ&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [4](https://www.youtube.com/watch?v=kKYY_OXGHxY&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [5](https://www.youtube.com/watch?v=esY21bNZEEg&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [6](https://www.youtube.com/watch?v=E0B-vSIT0Nc&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal) | [7](https://www.youtube.com/watch?v=Y9WgOTwQYXw&list=PLnC0w4gG7um3AyoNikU8t9vtkyYPlfTal)
 
 Week 7: **Logit Estimation**
 <!-- * [Slides](https://raw.githack.com/woerman/ResEcon703/master/slides/week_07/week_07.pdf) | [R code](https://raw.githack.com/woerman/ResEcon703/master/slides/week_07/week_07.R)
