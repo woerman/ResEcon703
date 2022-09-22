4cf5d6d2063ac88fe6b40d6af1abe2af055473ab
Matt Woerman <mwoerman@umass.edu>
2022-09-22 10:31:39 -0400

    week 4

---
+++ slides/week_04/week_04_slides.R
@@ -0,0 +1,49 @@
+########################################################
+##### ResEcon 703: Topics in Advanced Econometrics #####
+#####       Create figures for week 4 slides       #####
+#####         Matt Woerman, UMass Amherst          #####
+########################################################
+
+### Load packages --------------------------------------------------------------
+
+library(tidyverse)
+library(evd)
+
+
+### Logit Model ----------------------------------------------------------------
+
+### Plot type I extreme value density and distribution
+## Plot density of type I extreme value
+ev_pdf <- tibble(x = seq(-5, 5, .01), y = dgumbel(x)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_line() +
+  xlab(NULL) +
+  ylab(NULL)
+## Output density plot
+ggsave('ev_pdf.pdf', ev_pdf, width = 3, height = 3)
+## Plot distribution of type I extreme value
+ev_cdf <- tibble(x = seq(-5, 5, .01), y = pgumbel(x)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_line() +
+  xlab(NULL) +
+  ylab(NULL)
+## Output distribution plot
+ggsave('ev_cdf.pdf', ev_cdf, width = 3, height = 3)
+
+### Plot logistic density and distribution
+## Plot density of logistic
+logistic_pdf <- tibble(x = seq(-5, 5, .01), y = dlogis(x)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_line() +
+  xlab(NULL) +
+  ylab(NULL)
+## Output density plot
+ggsave('logistic_pdf.pdf', logistic_pdf, width = 3, height = 3)
+## Plot distribution of logistic
+logistic_cdf <- tibble(x = seq(-5, 5, .01), y = plogis(x)) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_line() +
+  xlab(NULL) +
+  ylab(NULL)
+## Output distribution plot
+ggsave('logistic_cdf.pdf', logistic_cdf, width = 3, height = 3)
